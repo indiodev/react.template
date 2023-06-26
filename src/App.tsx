94c566f8986baa71943b9f601e38195008ef7baf
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-06-26 05:46:56 -0400

    :ambulance: Update tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ export function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<section className="bg-[#ffffff] dark:bg-[#242424] h-screen w-screen flex flex-col items-center justify-center text-zinc-900 dark:text-zinc-100">
+		<section className="bg-[#ffffff] dark:bg-[#242424] h-screen w-screen flex flex-col items-center justify-center text-[#213547] dark:text-[#ffffffde]">
 			<h1 className="text-[3.2em]">React.template</h1>
 			<p>Base React template on Vite + Tailwind</p>
 			<div className="py-4">
 				<button
-					className="rounded-xl bg-[#f9f9f9]  dark:bg-zinc-100 py-[0.6em] px-[1.2em] border border-transparent"
+					className="rounded-xl dark:bg-[#1a1a1a] py-[0.6em] px-[1.2em] border border-transparent hover:border-[#646cff] focus:outline-[4px]"
 					onClick={() => setCount((count) => count + 1)}
 				>
 					count is {count}
