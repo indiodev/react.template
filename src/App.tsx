266bdbc35001589583df3092fced4e369b4b3350
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-06-26 04:53:31 -0400

    :sparkles: Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Fragment, useState } from 'react';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<Fragment>
-			<h1>React.template</h1>
+		<section className="dark:bg-zinc-900 h-screen w-screen flex flex-col items-center justify-center text-zinc-900 dark:text-zinc-100">
+			<h1 className="text-[3.2em]">React.template</h1>
 			<p>Base default on React + Vite</p>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+			<div className="py-4">
+				<button
+					className="rounded-xl bg-[#1a1a1a] text-zinc-100 dark:text-[#1a1a1a] dark:bg-zinc-100 py-[0.6em] px-[1.2em] border border-transparent dark:hover:border-[#646cff]"
+					onClick={() => setCount((count) => count + 1)}
+				>
 					count is {count}
 				</button>
 			</div>
-		</Fragment>
+		</section>
 	);
 }
 
