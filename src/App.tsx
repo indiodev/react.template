20adb496901ee41c678b7397ba76c0070fd9c1c7
Jhollyfer Rodrigues <jhollyfer.fr@gmail.com>
2023-06-26 04:55:57 -0400

    :art: Update

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
 	return (
 		<section className="dark:bg-zinc-900 h-screen w-screen flex flex-col items-center justify-center text-zinc-900 dark:text-zinc-100">
 			<h1 className="text-[3.2em]">React.template</h1>
-			<p>Base default on React + Vite</p>
+			<p>Base React template on Vite + Tailwind</p>
 			<div className="py-4">
 				<button
-					className="rounded-xl bg-[#1a1a1a] text-zinc-100 dark:text-[#1a1a1a] dark:bg-zinc-100 py-[0.6em] px-[1.2em] border border-transparent dark:hover:border-[#646cff]"
+					className="rounded-xl bg-[#1a1a1a] text-zinc-100 dark:text-[#1a1a1a] dark:bg-zinc-100 py-[0.6em] px-[1.2em] border border-transparent"
 					onClick={() => setCount((count) => count + 1)}
 				>
 					count is {count}
